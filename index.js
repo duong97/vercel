fa99b5a93630da15096543a1e43997ecdc0f1f7a
DuongNV <duongnv@kynaforkids.vn>
2024-08-31 01:32:25 +0700

    fetch

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const fetch = require('node-fetch');
 
 const app = express();
 const port = process.env.PORT || 3000;
