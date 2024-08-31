744150f17fc4cd90e25f224c6d07d96d2a069979
DuongNV <duongnv@kynaforkids.vn>
2024-08-31 23:29:14 +0700

    test

--- index.js
+++ index.js
@@ -2,47 +2,50 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
-const port = process.env.PORT || 80;
-
-const TELEGRAM_API = `https://api.telegram.org/bot6748805683:AAFHHYURGZmyCRVoNPEAnoW-_woBr-Qlfcg`;
+const port = process.env.PORT || 3000;
+const TELEGRAM_API_KEY = process.env.TELEGRAM_API_KEY;
+const TELEGRAM_API = `https://api.telegram.org/bot` + TELEGRAM_API_KEY;
 
 // Middleware to parse JSON bodies
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.statusCode = 200;
-  const msg = 'Hello Node!\n'
-  res.end(msg);
+    res.statusCode = 200;
+    const msg = 'Telegram bot command is working!\n'
+    res.end(msg);
 });
 
 // Handle incoming webhook requests
 app.post('/webhook', (req, res) => {
-  const message = req.body.message;
-  if (message) {
-    const chatId = message.chat.id;
-    const text = message.text;
+    const message = req.body.message;
+    if (message) {
+        const chatId = message.chat.id;
+        const text = message.text;
 
-    // Echo the received message back to the user
-    sendMessage(chatId, `You said: ${text}`);
-  } else {
-    sendMessage('-4045813885', 'no oke');
-  }
+        // Echo the received message back to the user
+        let msg = 'Default message';
+        if (text.startsWith('/help')) {
+            msg = "List command available:\n";
+            msg += "\\help: show list command!\n";
+        } else if (text.startsWith('/')) {
+            msg = "Unknown command!\n";
+        }
+        sendMessage(chatId, msg);
+    }
 
-  sendMessage('-4045813885', JSON.stringify(message));
-  // res.sendStatus(200); // Respond with a 200 status code
-  res.status(200).send({
-    result: 'Hello Node!',
-  }); // Respond with a 200 status code
+    res.status(200).send({
+        result: 'Success',
+    }); // Respond with a 200 status code
 });
 
 // Function to send a message to a Telegram chat
 function sendMessage(chatId, text) {
-  fetch(`${TELEGRAM_API}/sendMessage?` + new URLSearchParams({chat_id: chatId, text: text}).toString()).then(r => {
-    // console.log(r);
-  });
+    fetch(`${TELEGRAM_API}/sendMessage?` + new URLSearchParams({chat_id: chatId, text: text}).toString()).then(r => {
+        // console.log(r);
+    });
 }
 
 // Start the server
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
